6b4f18970b223b23412054297edbc9039ff341a4
cdobbyn <cdobbyn@gmail.com>
2014-12-18 19:04:14 -0800

    Added ip & port to the table.

--- networkcontrol/admin.py
+++ networkcontrol/admin.py
@@ -3,6 +3,8 @@ from networkcontrol.models import Switch
 
 
 class SwitchAdmin(admin.ModelAdmin):
-    list_display = ('name', 'provider')
+    list_display = ('name', 'provider', 'ip', 'port')
+
+#ip, port
 
 admin.site.register(Switch, SwitchAdmin)

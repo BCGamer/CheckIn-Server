bc4fb5c095a7191e0e7607b15abda204b735bf67
cdobbyn <cdobbyn@gmail.com>
2014-12-18 23:46:46 -0800

    Added "swichForm" model form, overrides the default textbox functionality with a PasswordInput field.
    Added "vlan1", "vlan2", "enabled" to switch table.
    
    Configured a basic fieldset to clean up some of the data.

--- networkcontrol/admin.py
+++ networkcontrol/admin.py
@@ -1,10 +1,28 @@
 from django.contrib import admin
 from networkcontrol.models import Switch
+from django import forms
+
+
+class switchForm(forms.ModelForm):
+    class Meta:
+        model = Switch
+        widgets = {
+            'password': forms.PasswordInput(render_value=True),
+        }
+        fields = ('password',)
 
 
 class SwitchAdmin(admin.ModelAdmin):
-    list_display = ('name', 'provider', 'ip', 'port')
+    list_display = ('name', 'provider', 'ip', 'port', 'vlan1', 'vlan2', 'enabled')
+    ordering = ('ip',)
+    form = switchForm
 
-#ip, port
+    fieldsets = (
+        (None, {'fields': ('name', 'provider', 'enabled')}),
+        ('Connectivity', {'fields': ('ip', 'port')}),
+        ('Authentication', {'fields': ('username', 'password')}),
+        ('VLAN', {'fields': ('vlan1', 'vlan2')}),
+    )
+
 
 admin.site.register(Switch, SwitchAdmin)

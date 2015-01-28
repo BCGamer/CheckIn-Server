ce543bcabfb62c85bfe6fc738e856bee67cfb9b9
cdobbyn <cdobbyn@gmail.com>
2015-01-27 23:29:12 -0800

    test_snmp_mac now returns name and val only if 'mac' matches 'val'. This is the basic functionality for checking if 'mac' is on the switch.

--- network/management/commands/test_snmp_mac.py
+++ network/management/commands/test_snmp_mac.py
@@ -2,6 +2,7 @@ import logging
 
 from pysnmp.entity.rfc3413.oneliner import cmdgen
 from network.models import Switch
+from netaddr import *
 
 from django.core.management.base import BaseCommand, CommandError
 
@@ -12,30 +13,32 @@ class Command(BaseCommand):
     help = 'Does some switch stuffs'
 
     def handle(self, *args, **options):
+        mac_matches = {}
         switch = Switch.objects.get(id=1)
 
+        mac = '001f.161c.4c86'
         ip = switch.ip
-        security_name = switch.snmp_username
-        auth_key = switch.snmp_auth_pass
-        privacy_key = switch.snmp_priv_pass
 
-        mac_address_list = (1,3,6,1,2,1,17,4,3,1,1)
+        snmp_user = cmdgen.UsmUserData(
+            switch.snmp_username,
+            switch.snmp_auth_pass,
+            switch.snmp_priv_pass,
+            authProtocol=cmdgen.usmHMACSHAAuthProtocol,
+            privProtocol=cmdgen.usmAesCfb128Protocol)
 
+        snmp_target = cmdgen.UdpTransportTarget((ip, 161))
 
-        #value = ('.1.3.6.1.6.3.1.1.5.1')
+        oid_mac = (1,3,6,1,2,1,17,4,3,1,1)
+        oid_ifindex = (1,3,6,1,2,1,2,2,1,1)
+        oid_port = (1,3,6,1,2,1,31,1,1,1,1)
 
         # Re-use this as much as possible!!
-        cmdGen = cmdgen.CommandGenerator()
+        comm = cmdgen.CommandGenerator()
 
-        errorIndication, errorStatus, errorIndex, varBindTable = cmdGen.nextCmd(
-            cmdgen.UsmUserData(
-                security_name, auth_key, privacy_key,
-                authProtocol=cmdgen.usmHMACSHAAuthProtocol,
-                privProtocol=cmdgen.usmAesCfb128Protocol
-            ),
-            cmdgen.UdpTransportTarget((ip, 161)),
-
-            mac_address_list
+        # Check for MAC addresses that match
+        errorIndication, errorStatus, errorIndex, varBindTable = comm.nextCmd(
+            snmp_user, snmp_target,
+            (1,3,6,1,2,1,17,4,3,1,1)
         )
 
         if errorIndication:
@@ -48,6 +51,43 @@ class Command(BaseCommand):
                     )
                 )
             else:
+                # Loop through all
                 for varBindTableRow in varBindTable:
                     for name, val in varBindTableRow:
-                        print('%s = %s' % (name.prettyPrint(), val.prettyPrint()))
+                        val = val.prettyPrint()[2:]
+                        val = EUI(val, dialect=mac_cisco)
+
+                        # get oid name (as object instance) like: 1.3.6.1.2.1.17.4.3.1.1.0.20.105.84.18.25
+                        # modify name so it doesn't include oid 1.3.6.1.2.1.17.4.3.1.1
+                        # return output as tuple
+                        name = str(name)[23:]   # Strip 1.3.6.1.2.1.17.4.3.1.1.
+                        name = tuple(name.split("."))
+
+                        if val == EUI(mac, dialect=mac_cisco):
+                            mac_matches[name] = val
+                            print "%s = %s" % (name, val)
+
+        #
+
+    def snmp_locate_bridge(self, comm, snmp_user, snmp_target, oid):
+        oid_bridge = (1,3,6,1,2,1,17,4,3,1,2)
+
+        errorIndication, errorStatus, errorIndex, varBindTable = comm.nextCmd(
+            snmp_user, snmp_target, oid_bridge
+        )
+
+        if errorIndication:
+            print(errorIndication)
+        else:
+            if errorStatus:
+                print('%s at %s' % (
+                    errorStatus.prettyPrint(),
+                    errorIndex and varBindTable[-1][int(errorIndex)-1] or '?'
+                    )
+                )
+            else:
+                # Loop through all
+                for varBindTableRow in varBindTable:
+                    for name, val in varBindTableRow:
+                        val = val.prettyPrint()[2:]
+                        val = EUI(val, dialect=mac_cisco)
